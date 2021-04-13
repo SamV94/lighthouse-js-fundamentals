f5dafda385d574f4bc3dba2587102e644ba5942e
Sam Vardanian <sam.vardanian@hotmail.ca>
2021-04-13 18:13:42 -0400

    More conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,7 @@
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
+
